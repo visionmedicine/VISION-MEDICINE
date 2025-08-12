ece05b1021137289f462898f6cbef5fb976332e6
alfitranurr <alfitranurr@gmail.com>
2025-08-12 10:42:46 +0700

    update ukuran jarak page pov hp

--- WEBSITE/frontend/src/pages/Home.tsx
+++ WEBSITE/frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <Box p={3} pl={{ base: 16, md: 4 }}>
+    <Box p={3} pl={{ base: 12, md: 4 }}>
       <Heading fontSize={{ base: "xl", md: "2xl" }}>Home Page</Heading>
       <Text fontSize={{ base: "sm", md: "md" }}>
         Welcome to Vision Medicine!
--- WEBSITE/frontend/src/pages/MedicineInformation.tsx
+++ WEBSITE/frontend/src/pages/MedicineInformation.tsx
@@ -2,7 +2,7 @@ import { Box, Heading } from "@chakra-ui/react";
 
 const MedicineInformation = () => {
   return (
-    <Box p={3} pl={{ base: 16, md: 4 }}>
+    <Box p={3} pl={{ base: 12, md: 4 }}>
       <Heading fontSize={{ base: "xl", md: "2xl" }}>
         Medicine Information
       </Heading>
