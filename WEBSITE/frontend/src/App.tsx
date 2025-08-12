371f339f5ea05239fbd8dd8a1f44e1af86b9ca2e
alfitranurr <alfitranurr@gmail.com>
2025-08-12 16:04:18 +0700

    App.tsx

--- WEBSITE/frontend/src/App.tsx
+++ WEBSITE/frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.tsx";
 import { Suspense } from "react";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <Suspense fallback={<div>Loading...</div>}>
         <RouterProvider router={router} />
       </Suspense>
