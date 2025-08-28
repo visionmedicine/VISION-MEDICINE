0bd5d6107ee880d53e31d050e065193df9cc7c48
alfitranurr <alfitranurr@gmail.com>
2025-08-28 10:55:25 +0700

    tampilan mobile

--- WEBSITE/frontend/src/components/layouts/MobileLayout.tsx
+++ WEBSITE/frontend/src/components/layouts/MobileLayout.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer/Footer.tsx";
 
 const MobileLayout: React.FC = () => {
   return (
-    <Flex direction="column" minH="100vh">
+    <Flex direction="column" minH="100dvh">
       <MobileSidebar />
       <Box flex="1" p={2}>
         <Outlet />
